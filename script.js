b01d18417c84796a4fed317ce57cf70326f20dd6
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-22 15:18:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 //your JS code here. If required.
 var select = document.getElementById("colorSelect");
-var val;
-function selectedVal(){
-    val = select.value;
-}
 
 function remove(){
-    for(var i=0; i<select.length; i++){
-        if(select.options[i].value === val){
-            select.remove(i);
-            break;
-        }
-    }
+	var index = select.selectedIndex;
+	select.remove(index);
 }
+
+	
+
